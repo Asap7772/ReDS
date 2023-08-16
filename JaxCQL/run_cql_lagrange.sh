baec4db57bc1a0e551e5a546de120914229087dc
Anikait Singh <singh.anikait@gmail.com>
2023-08-15 19:32:56 -0700

    orig antmaze

--- JaxCQL/run_cql_lagrange.sh
+++ JaxCQL/run_cql_lagrange.sh
@@ -1,26 +1,23 @@
 envs=(antmaze-medium-diverse-v0 antmaze-medium-play-v0)
-tau=(0.0001 0.01 0.1 1)
-cql_target_action_gaps=(0.1 0.5 0.8 2)
+cql_target_action_gaps=(0.8)
 seeds=(42)
 num_exps=0
 gpus=(0 1 2 3 4 5 6 7)
 
 for seed in "${seeds[@]}"; do
 for env in "${envs[@]}"; do
-for t in "${tau[@]}"; do
 for a in "${cql_target_action_gaps[@]}"; do
     which_gpu=$((num_exps % ${#gpus[@]}))
     gpu=${gpus[$which_gpu]}
     export CUDA_VISIBLE_DEVICES=$gpu
 
     echo "env: $env, seed: $seed, tau: $t, alpha: $a, gpu: $gpu"
-    command="XLA_PYTHON_CLIENT_PREALLOCATE=false python -m JaxCQL.conservative_sac_reds_main \
+    command="XLA_PYTHON_CLIENT_PREALLOCATE=false python -m JaxCQL.conservative_sac_main \
     --env $env \
     --logging.output_dir './experiment_output' \
     --logging.online True \
-    --logging.project 'ReDS_antmaze_arch' \
+    --logging.project 'CQL_antmaze_arch' \
     --seed $seed \
-    --cql.reds_temp $t \
     --cql.cql_lagrange True \
     --cql.cql_min_q_weight=5.0 \
     --cql.cql_target_action_gap=0.8 \
@@ -35,7 +32,6 @@ for a in "${cql_target_action_gaps[@]}"; do
 
     sleep 30 # wait for the experiment to start
     num_exps=$((num_exps+1))
-done
 done
 done
 done
